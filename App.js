b518f2fd6aac12e20e36c5ff293af3dc60065cc3
NamHey99 <112053839+NamHey99@users.noreply.github.com>
2024-04-22 15:54:26 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Pham Ngoc Nam.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ha ha
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ demo.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+
+function demo() {
+    return (
+        <div>
+          <h1> Hello Reactjs </h1>
+          <p> What </p>
+        </div>
+    ); 
+}
+export default demo;
